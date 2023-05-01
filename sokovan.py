b69146a7a4ee480d21f1dd95eebb5d59886c7b54
Natalia Marin <natalia.andrea.marin@correounivalle.edu.co>
2023-05-01 15:33:22 -0500

    update v.2.0

--- sokovan.py
+++ sokovan.py
@@ -1,109 +1,148 @@
+from collections import deque
 import os
-import busqueda
 
-#Lee el mapa que se le entrega en el input y lo asume como mapa
-def leerMapa():    
-    mapa = input("¿Qué nivel jugaré?")
+def leerMapa():
+    mapa = input("¿Qué nivel jugaré? ")
     nivel_ruta = os.path.join("nivelesAqui", mapa)
     with open(f"nivelesAqui/{mapa}.txt", "r") as file:
         content = file.readlines()
-    content = [linea.strip().split() for linea in content]
-    # Mapear los caracteres especiales a sus valores numéricos
-    bloque_mapeo = {'W': 1, 'X': 2, '0': 0}
+    content = [linea.strip() for linea in content]
+    filas = len(content)
+    columnas = len(content[0])
+    mapa = []
     jugador_pos = None
     cajas_pos = []
-    for i in range(len(content)):
-        for j in range(len(content[i])):
-            if content[i][j] == '1':
-                if jugador_pos is not None:
-                    return None, None, None # hay más de un jugador
-                jugador_pos = (i, j)
-            elif content[i][j].isdigit() and int(content[i][j]) > 1:
-                caja_pos = (i, j)
-                cajas_pos.append(caja_pos)
-            content[i][j] = bloque_mapeo[content[i][j]]
+    cajasEnObjetivo = 0
+    for i in range(filas):
+        fila = []
+        for j in range(columnas):
+            if content[i][j] == 'W':
+                fila.append(1) # pared
+            elif content[i][j] == '0':
+                fila.append(0) # camino
+            elif content[i][j] == 'X':
+                fila.append(2) # objetivo
+                cajasEnObjetivo += 1
+            else:
+                fila.append(0) # camino
+                if content[i][j] == 'P':
+                    if jugador_pos is not None:
+                        return None, None, None, None, None # hay más de un jugador
+                    jugador_pos = (i, j)
+                elif content[i][j] == 'C':
+                    caja_pos = (i, j)
+                    cajas_pos.append(caja_pos)
+        mapa.append(fila)
     if jugador_pos is None:
-        return None, None, None # no hay jugador
-    return content, jugador_pos, cajas_pos
+        return None, None, None, None, None # no hay jugador
+    cajas = len(cajas_pos)
+    return mapa, jugador_pos, cajas_pos, cajasEnObjetivo, cajas
 
 nivel = leerMapa()
 
-def contar_cajas(ruta_archivo):
-    with open(ruta_archivo, "r") as file:
-        content = file.readlines()
-    numCajas = sum([1 for linea in content if "," in linea]) - 1
-    return numCajas if numCajas > 0 else None
+def mover_jugador(mapa, jugador_pos, cajas_pos, direccion):
+    # definir cambios en fila y columna para cada dirección
+    cambios = {
+        "U": (-1, 0),
+        "D": (1, 0),
+        "L": (0, -1),
+        "R": (0, 1),
+    }
+    fila_jugador, col_jugador = jugador_pos
+    # calcular la posición a la que se quiere mover el jugador
+    fila_nueva = fila_jugador + cambios[direccion][0]
+    col_nueva = col_jugador + cambios[direccion][1]
+    # si la posición a la que se quiere mover es una pared, no se puede mover
+    if mapa[fila_nueva][col_nueva] == 1:
+        return mapa, jugador_pos, cajas_pos
+    # si hay una caja en la posición a la que se quiere mover
+    if (fila_nueva, col_nueva) in cajas_pos:
+        # calcular la posición detrás de la caja
+        fila_caja_nueva = fila_nueva + cambios[direccion][0]
+        col_caja_nueva = col_nueva + cambios[direccion][1]
+        # si la posición detrás de la caja es una pared o ya hay una caja ahí, no se puede mover
+        if mapa[fila_caja_nueva][col_caja_nueva] == 1 or (fila_caja_nueva, col_caja_nueva) in cajas_pos:
+            return mapa, jugador_pos, cajas_pos
+        # mover la caja y actualizar su posición en la lista de posiciones de cajas
+        cajas_pos.remove((fila_nueva, col_nueva))
+        cajas_pos.append((fila_caja_nueva, col_caja_nueva))
+    # mover el jugador y actualizar su posición
+    jugador_pos = (fila_nueva, col_nueva)
+    # actualizar el mapa
+    mapa[fila_jugador][col_jugador] = 0
+    mapa[fila_nueva][col_nueva] = 3
+    for caja_pos in cajas_pos:
+        mapa[caja_pos[0]][caja_pos[1]] = 2
+    return mapa, jugador_pos, cajas_pos
 
-numCajas = contar_cajas(nivel)
 
-posicion_caja_n = [i for i in range(len(nivel)) for j in range(len(nivel[i])) if isinstance(nivel[i][j], list) and nivel[i][j][0] == numCajas][0:2]
-posicion_caja_1 = [i for i in range(len(nivel)) for j in range(len(nivel[i])) if isinstance(nivel[i][j], list) and nivel[i][j][0] == 1][0:2]
+def dfs(mapa, jugador_pos, cajas_pos, cajasEnObjetivo):
+    visitados = set()
+    pila = [(jugador_pos, cajas_pos)]
+    while pila:
+        pos_jugador, pos_cajas = pila.pop()
+        if all(caja in cajasEnObjetivo for caja in pos_cajas):
+            return [] # Todas las cajas están en los objetivos
+        if (pos_jugador, pos_cajas) in visitados:
+            continue
+        visitados.add((pos_jugador, pos_cajas))
+        for movimiento in ["U", "D", "L", "R"]:
+            nuevo_mapa, nuevo_pos_jugador, nuevo_pos_cajas = mover_jugador(mapa, pos_jugador, pos_cajas, movimiento)
+            if (nuevo_pos_jugador, nuevo_pos_cajas) not in visitados:
+                pila.append((nuevo_pos_jugador, nuevo_pos_cajas, movimiento))
 
-#Analiza los movimientos que el jugador/IA tiene disponibles a su alrededor y genera una lista con ellos 
+def bfs(mapa, jugador_pos, cajas_pos, cajasEnObjetivo):
+    visitados = set()
+    cola = deque([(jugador_pos, cajas_pos, [])])
+    while cola:
+        pos_jugador, pos_cajas, movimientos = cola.popleft()
+        if all(caja in cajasEnObjetivo for caja in pos_cajas):
+            return movimientos # Todas las cajas están en los objetivos
+        if (pos_jugador, pos_cajas) in visitados:
+            continue
+        visitados.add((pos_jugador, pos_cajas))
+        for movimiento in ["U", "D", "L", "R"]:
+            nuevo_mapa, nuevo_pos_jugador, nuevo_pos_cajas = mover_jugador(mapa, pos_jugador, pos_cajas, movimiento)
+            if (nuevo_pos_jugador, nuevo_pos_cajas) not in visitados:
+                cola.append((nuevo_pos_jugador, nuevo_pos_cajas, movimientos + [movimiento]))
 
-def movimientosDisponibles(nivel, jugador_pos):
+def iddfs(mapa, jugador_pos, cajas_pos, cajasEnObjetivo):
+    for profundidad_maxima in range(1, 100):
+        resultado = dfs_limitado(mapa, jugador_pos, cajas_pos, cajasEnObjetivo, profundidad_maxima)
+        if resultado is not None:
+            return resultado
+
+def dfs_limitado(mapa, jugador_pos, cajas_pos, cajasEnObjetivo, profundidad_maxima):
+    visitados = set()
+    pila = [(jugador_pos, cajas_pos, [])]
+    while pila:
+        pos_jugador, pos_cajas, movimientos = pila.pop()
+        if all(caja in cajasEnObjetivo for caja in pos_cajas):
+            return movimientos # Todas las cajas están en los objetivos
+        if (pos_jugador, pos_cajas) in visitados:
+            continue
+
+
+def obtener_movimientos(mapa, pos):
     movimientos = []
-    filas = len(nivel)
-    columnas = len(nivel[0])
-    fila_jugador, columna_jugador = jugador_pos
-
-    if fila_jugador > 0 and nivel[fila_jugador-1][columna_jugador] == 0:
-        movimientos.append('U')
-    if fila_jugador < filas-1 and nivel[fila_jugador+1][columna_jugador] == 0:
-        movimientos.append('D')
-    if columna_jugador > 0 and nivel[fila_jugador][columna_jugador-1] == 0:
-        movimientos.append('L')
-    if columna_jugador < columnas-1 and nivel[fila_jugador][columna_jugador+1] == 0:
-        movimientos.append('R')
+    filas, columnas = len(mapa), len(mapa[0])
+    x, y = pos
+    
+    # Movimiento hacia arriba
+    if x > 0 and mapa[x-1][y] != 1:
+        movimientos.append((x-1, y, "U"))
+    
+    # Movimiento hacia abajo
+    if x < filas-1 and mapa[x+1][y] != 1:
+        movimientos.append((x+1, y, "D"))
+    
+    # Movimiento hacia la izquierda
+    if y > 0 and mapa[x][y-1] != 1:
+        movimientos.append((x, y-1, "L"))
+    
+    # Movimiento hacia la derecha
+    if y < columnas-1 and mapa[x][y+1] != 1:
+        movimientos.append((x, y+1, "R"))
     
     return movimientos
 
-
-#Jerarquía con la que preferirá ir si puede realizar dos o más movimientos en un punto
-def jerarquia():
-    return ['U', 'D', 'L', 'R']
-
-#compara jerarquia con movimientos y si el primer elemento de jerarquía está en movimientos, entonces elige ese elemento.
-def elegirMovimiento(movimientos_posibles, jerarquia):
-    for movimiento in jerarquia:
-        if movimiento in movimientos_posibles:
-            return movimiento
-    print("No puedo moverme. Help")
-    return None
-
-def resolverNivel():
-    # Leer el mapa
-    nivel, jugador_pos, cajas_pos = leerMapa()
-    if nivel is None:
-        print("Error: El archivo de nivel es inválido.")
-        return
-
-    # Contar cajas y definir el estado del juego
-    numCajas = contar_cajas(nivel)
-    cajasEnObjetivo = 0
-    restantes = numCajas
-
-    # Resolver el nivel
-    movimientos = []
-    while cajasEnObjetivo < numCajas:
-        # Mostrar el nivel y preguntar por el siguiente movimiento
-        leerMapa(nivel, jugador_pos, cajas_pos)
-        movimientos_posibles = movimientosDisponibles(nivel, jugador_pos[0], jugador_pos[1])
-        movimiento = elegirMovimiento(movimientos_posibles, ["U", "D", "L", "R", ""])
-        if movimiento is None:
-            print("No hay movimientos disponibles.")
-            break
-
-        # Mover el jugador y actualizar la posición de las cajas
-        if movimiento != "":
-            movimientos.append(movimiento)
-        nivel, jugador_pos, cajas_pos, caja_empujada = elegirMovimiento(nivel, jugador_pos, cajas_pos, movimiento)
-
-        # Actualizar el estado del juego
-        if caja_empujada:
-            if nivel[caja_empujada[0]][caja_empujada[1]] == 2:
-                cajasEnObjetivo += 1
-                restantes -= 1
-
-    # Imprimir los movimientos usados
-    print(movimientos)
